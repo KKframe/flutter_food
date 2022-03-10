bdef921cecec05bde46670dfd4654998558143ed
KKframe <keawsung.fame@gmail.com>
2022-03-10 20:47:31 +0700

    Initial commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,124 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //Map<String,dynamic> foodMap = {};
+  List<dynamic> data=[];
+  List<String> name = [];
+  List<int> price = [];
+  List<String> img = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('FLUTTER FOOD')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _handleClickButton, //onPress ต้องใส่เป็นฟังก์ชัน ไม่ใช่ result จากฟังก์ชัน
+                  child: const Text('LOAD FOODS DATA'),
+                ),
+              ],
+            ),
+          ),
+          if(data.isNotEmpty)
+
+            /*for(var i in  data)
+              foodMap.addAll(data[i]);
+              buildFoodList(),*/
+            Flexible(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: data.length,
+                  physics: ScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    var i = index;
+                    return buildFoodList(i);
+                  }
+              ),
+            )
+
+        ],
+      ),
+    );
+  }
+
+  Card buildFoodList(int i) {
+    return Card(
+      elevation: 5.0,
+            child: Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.network(img.elementAt(i),width: 100,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            Text(name.elementAt(i),style: TextStyle(fontSize: 20,color: Colors.pink.shade500),),
+                            Text(price.elementAt(i).toString(),style: TextStyle(fontSize: 15,color: Colors.pink.shade200,),),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+  }
+  Future<void> _handleClickButton() async {
+    final url = Uri.parse('https://cpsu-test-api.herokuapp.com/foods');
+    var result = await http.get(url);  //await คือ การแปลงเป็น result.then()ให้
+    print(result.body);
+    var jj = jsonDecode(result.body); // decode เพื่อแปลงออกมาเป็นโครงสร้างในภาษา dart จะได้ List ของ Map
+    String status = jj['status']; //jj['ชื่อ key']
+    String? msg = jj['message'];
+
+    setState((){
+      data = jj['data'];
+      print('Status : $status , Message : $msg, Number of food : ${data.length}');//
+      Map<String,dynamic> ts={};
+      for(var i in data){
+        ts = i;
+       print('menu ${ts['name']} price ${ts['price']} img ${ts['image']}');
+       name.add(ts['name']);
+       price.add(ts['price']);
+       img.add(ts['image']);
+      }
+
+    });
+
+    //result.then((response) => print(response.body)); //(response) => print(response.body) คือ นิพจน์ฟังก์ชัน
+    //result.then(_handleResponse); //(response) => print(response.body) คือ นิพจน์ฟังก์ชัน
+
+    //var i =0;
+    //Above code is 'Asynchronous programming'
+    /*   EX. โปรแกรมเรียกฟังก์ชัน A(); และ B();
+    โปรแกรมจะรันฟังก์ชัน A(); และ B(); ตามลำดับ
+    โดยไม่สนใจว่าฟังก์ชัน A(); จะทำงานเสร็จรึยัง
+    จะไปเรียกฟังก์ชัน B(); ต่อเลยทันที*/
+  }
+ /* void _handleResponse(http.Response response){
+    print(response.body);
+  }*/
+}
